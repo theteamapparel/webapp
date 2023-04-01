910a780cd07512fc4ac839a9ce8dd687f17882b2
Abubakar <abubakarlawan112@gmail.com>
2023-04-01 13:03:26 +0100

    first commit

--- setup/settings.py
+++ setup/settings.py
@@ -14,7 +14,7 @@ import os
 from pathlib import Path
 
 
-WEBSITE_ADDRESS="https://theteamapparel.com"
+WEBSITE_ADDRESS="http://191.101.81.148"
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
